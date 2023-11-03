fa5080b3bbd3747e98c41af72741ebd53984c030
Dongg0210 <donggrangddang2@gmail.com>
2023-11-03 19:31:10 +0900

    ChessBoard

--- chessengine.py
+++ chessengine.py
@@ -1,69 +1,87 @@
+'''
+White : True
+Black : False
+turn :  짝수 = 백차례
+        홀수 = 흑차례
+'''
+
+
 import chess
 
-def trans_position(x,y):
-    if x == 0:
-        trans_x = "a"
-    elif  x == 1:
-        trans_x = "b"
-    elif  x == 2:
-        trans_x = "c"
-    elif  x == 3:
-        trans_x = "d"
-    elif  x == 4:
-        trans_x = "e"
-    elif  x == 5:
-        trans_x = "f"
-    elif  x == 6:
-        trans_x = "g"
-    elif  x == 7:
-        trans_x = "h"
+class ChessBoard:
 
-    trans_y = str(8 - y)
+    def __init__(self, board):
+        self.board = chess.Board
 
-    return "".join([trans_x, trans_y])
+    def transPosition(x,y):
+        if x == 0:
+            trans_x = "a"
+        elif  x == 1:
+            trans_x = "b"
+        elif  x == 2:
+            trans_x = "c"
+        elif  x == 3:
+            trans_x = "d"
+        elif  x == 4:
+            trans_x = "e"
+        elif  x == 5:
+            trans_x = "f"
+        elif  x == 6:
+            trans_x = "g"
+        elif  x == 7:
+            trans_x = "h"
 
-def judgement(board):
-    if board.is_stalemate() == True:
+        trans_y = str(8 - y)
+
+        return "".join([trans_x, trans_y])
+
+    def judgement(board, turn):
+        if board.is_stalemate() == True:
+
+            if turn % 2 == 0:
+                return True, True
+            else:
+                return 
+            
+
+        elif board.is_checkmate() == True:
+
+            return "Checkmate"
+
+    def legal_move(board, position):
+
+        if chess.Move.from_uci(position) in board.legal_moves:
+            return True
         
-        return "Stalemate"
+        return False
 
-    elif board.is_checkmate() == True:
+    def move(board, raw_position):
 
-        return "Checkmate"
+        raw_position = raw_position.split(",")
 
-def legal_move(board, position):
+        x = int(raw_position[0])
+        y = int(raw_position[1])
+        next_x = int(raw_position[2])
+        next_y = int(raw_position[3])
+        position = "".join([trans_position(x, y), trans_position(next_x, next_y)])
+        if legal_move(board, position) == True:
+            board.push_san(position)
+            print(f'{position} success')
+            return True, board
+        else:
+            print(f'{position} failed')
+            return False, board
 
-    if chess.Move.from_uci(position) in board.legal_moves:
-        return True
-    
-    return False
+    def return_reward(list):
+        if list[0] == True:
+            return 0
+        else:
+            return -0.1
 
-def move(board, raw_position):
-
-    raw_position = raw_position.split(",")
-
-    x = int(raw_position[0])
-    y = int(raw_position[1])
-    next_x = int(raw_position[2])
-    next_y = int(raw_position[3])
-    position = "".join([trans_position(x, y), trans_position(next_x, next_y)])
-
-
-
-
-def run():
-
-    board = chess.Board()
-
-
-    if legal_move(board, position) == True:
-        board.push_san(position)
-        print(f'{position} success')
-
-    else:
-        print(f'{position} failed')
-
-    print(board)
+    def run(board):
+        board = 
+            
+        
         
 
 if __name__ == '__main__':
